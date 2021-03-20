65957e23e4cb48c1f690738c8ef087ac5c5f5bef
radioactive11 <roy.arijit2001@gmail.com>
2021-03-20 16:19:08 +0530

    bst: insert & inorder

---
+++ DS/BST/bst.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+
+using namespace std;
+
+class bst
+{
+private:
+    bst * right;
+    bst * left;
+    int data;
+public:
+    bst();
+    bst(int data);
+    void inorder_traversal(bst* root);
+    bst* insert_node(bst* root, int value);
+    ~bst();
+};
+
+bst::bst()
+{
+    cout << "Afforestation\n";
+}
+
+bst::bst(int data)
+{
+    this->right = nullptr;
+    this->left = nullptr;
+    this->data = data;
+}
+
+void bst::inorder_traversal(bst* root)
+{
+    if(root == nullptr)
+        return;
+    
+    else
+    {
+        inorder_traversal(root->left);
+        cout << root->data << "\n";
+        inorder_traversal(root->right);
+    }
+}
+
+bst* bst::insert_node(bst* root, int value)
+{
+    
+    if (root == nullptr)
+    {
+        return new bst(value);
+    }
+    
+    if(value < root->data)
+    {
+        root->left = insert_node(root->left, value);
+    }
+
+    else
+    {
+        root->right = insert_node(root->right, value);
+    }
+
+    return root;
+}
+
+
+bst::~bst()
+{
+    cout << "Deforestation\n";
+}
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    bst B;
+    bst * root = nullptr;
+    root = B.insert_node(root, 30);
+    root = B.insert_node(root, 20);
+    root = B.insert_node(root, 40);
+    root = B.insert_node(root, 10);
+    root = B.insert_node(root, 80);
+    root = B.insert_node(root, 70);
+    B.inorder_traversal(root);
+    return 0;
+}
